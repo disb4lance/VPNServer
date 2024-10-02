a973ab4af50d853f35e366b2615a585ec1034eda
disb4lance <ivan.shamzhurov.04@bk.ru>
2024-10-03 00:41:27 +0300

    Initial commit: Create ASP.NET Core VPN management service

---
+++ VPN Management/VPN Management.Presintation/Controllers/VPNController .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace VPN_Management.Presintation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VPNController : ControllerBase
+    {
+        [HttpGet(Name = "status")]
+        public IActionResult GetStatus()
+        {
+            return Ok("VPN server is running.");
+        }
+
+        [HttpPost("connect")]
+        public IActionResult ConnectVPN()
+        {
+            // Логика подключения VPN
+            return Ok("VPN connection initiated.");
+        }
+    }
+}
